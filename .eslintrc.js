8c1bbb91e26854a5c9b614e3cc1fd8a4d4aa7053
Luke Hu <hyt577@gmail.com>
2018-06-04 21:44:01 +0800

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,23 +35,4 @@ module.exports = {
 
     }
 };
-// module.exports = {
-//     "extends": ["eslint:recommended"],
-//     "rules": {
-//         "no-console": ["error", {
-//         "allow": ["warn", "error", "info"]
-//         }]
-//     },
-//     "parser": "babel-eslint",
-//     "parserOptions": {
-//         "ecmaVersion": 6,
-//         "sourceType": "script"
-//     },
-//     "globals": {
-//     },
-//     "env": {
-//         "node": true,
-//         "es6": true,
-//         "mocha": true
-//     }
-// };
+
