a8a9fed6e40e4e133fc0c98ee4a700fd59f6e1b5
Luke Hu <hyt577@gmail.com>
2018-06-04 21:33:59 +0800

    add pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,52 @@
+// module.exports = {
+//     "env": {
+//         "es6": true,
+//         "node": true
+//     },
+//     "extends": "eslint:recommended",
+//     "rules": {
+//         "parser": "babel-eslint",
+//         "indent": [
+//             "error",
+//             2
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "single"
+//         ],
+//         // "semi": [
+//         //     "error",
+//         //     "always"
+//         // ],
+//         "no-console": [
+//             "error",{
+//                 "allow": ["warn", "error", "info"]
+//             }
+//         ],
+//         "parser": "babel-eslint"
+//     }
+// };
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended"],
     "rules": {
-        "parser": "babel-eslint",
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        // "semi": [
-        //     "error",
-        //     "always"
-        // ],
-        "no-console": [
-            "error",{
-                "allow": ["warn", "error", "info"]
-            }
-        ],
-        "parser": "babel-eslint"
+        "no-console": ["error", {
+        "allow": ["warn", "error", "info"]
+        }]
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "script"
+    },
+    "globals": {
+    },
+    "env": {
+        "node": true,
+        "es6": true,
+        "mocha": true
     }
 };
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # anywhere
 Tiny NodeJS Static Web server
+
+## 安装
+
+```
+npm i -g anywhere
+```
+
+
+## 使用方法
+
+```
+anywhere  # 把当前文件夹作为静态资源服务器
+
+anywhere -p 8080  # 设置端口号为 8080
+
+anywhere -h localhost   # 设置 host 为 localhost
+
+anywhere -d /usr   # 将 /usr 设置为根目录
+
+```
